c72644987ef7d3670daed922c2c9b7a386f3d5d4
Hisham Ahmed <hisham.ahmed@aol.com>
2024-10-09 14:37:45 +0200

    ner_gpt

---
+++ ner_gpt.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import ast
+
+# Place your OpenAI key in a file called .openai_api_key
+
+api_key = open(".openai_api_key", "r").read()
+
+ner_prompt = open("NER_prompt.txt", "r").read()
+
+def ner_gpt(input_text):
+    client = OpenAI(api_key=api_key)
+    completion = client.chat.completions.create(
+        temperature=0.5,
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": ner_prompt
+            },
+            {
+                "role": "user",
+                "content": 
+                f"""
+                    Input text: {input_text},
+                """
+            }
+        ]
+    )
+    result = completion.choices[0].message.content
+    result_dict = ast.literal_eval(result)
+    return result_dict
